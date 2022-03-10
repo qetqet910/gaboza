ca5ceaeab86081668ada26e3bb316619e220b5c4
qetqet910 <79036088+qetqet910@users.noreply.github.com>
2022-03-10 10:28:28 +0900

    어제 깜빡했다

--- Mini_Project/script.js
+++ Mini_Project/script.js
@@ -1,15 +1,43 @@
 const CardsPro = document.querySelector(".CardProfile");
 const Cards = document.querySelector(".CardPocket");
-const OverDiv = document.querySelector(".overdiv");
+// const OverDiv = document.querySelector(".overdiv");
+const Lis = document.querySelectorAll(".CardPocket li");
+const LeftWindow = document.querySelector(".LeftWindow")
+const closeBtn = document.querySelector(".close");
 
-console.log(OverDiv);
+let Counts = 0;
+
+Lis.forEach(element => {
+    element.addEventListener('click', (e) => {
+        if (Counts == 0) {
+            CardOn(e)
+            // e.target.style.zIndex = '999';
+        }
+    }, false)
+});
+
+closeBtn.addEventListener('click', (e) => {
+    LeftWindow.style.left = "-100%";
+    LeftWindow.style.backgroundColor = "rgba(255, 255, 255, 0)";
+    closeBtn.style.opacity = '0';
+})
+
+function CardOn(e) {
+    let CardsColor = window.getComputedStyle(e.target).backgroundColor
+    LeftWindow.style.backgroundColor = `${CardsColor}`
+    LeftWindow.style.left = "0%";
+    closeBtn.style.opacity = '1';
+}
 
 function dragElement(elmnt) {
     let pos1, pos3, Res = 0;
     elmnt.onmousedown = dragMouseDown;
-
+    Counts = 1;
     function dragMouseDown(e) {
         e.preventDefault();
+        if (e.target.localName == "li") {
+            CardOn(e)
+        }
         pos3 = e.clientX;
         document.onmouseup = closeDragElement;
         document.onmousemove = elementDrag;
@@ -20,42 +48,14 @@ function dragElement(elmnt) {
         pos1 = pos3 - e.clientX;
         pos3 = e.clientX;
         Res += pos1;
-        Cards.style.transform = `rotate(${-(Res / 10)}deg)`;
+        Cards.style.transform = `rotate(${-100 - (Res / 10)}deg)`;
     }
 
     function closeDragElement() {
         document.onmouseup = null;
         document.onmousemove = null;
+        Counts = 0;
     }
 }
 
 dragElement(CardsPro);
-
-// const DragFunc = (ele) => {
-//     let poster, pos3, pos4;
-//     ele.onmousedown = dragMouseDown;
-
-//     function dragMouseDown(e) {
-//         e.preventDefault();
-//         poster = e.clientX
-//         document.onmousemove = elementDrag;
-//         document.onmouseup = closeDragElement;
-//     }
-
-//     function elementDrag(e) {
-//         e.preventDefault();
-//         // if()
-//         pos3 = e.clientX;
-//         pos4 = pos3;
-//         console.log(CardsPro.clientWidth / 2, e.clientX);
-//         // console.log(CardsPro.clientWidth / 2);
-//         Cards.style.transform = `rotate(${pos4 / 10}deg)`;
-//     }
-
-//     function closeDragElement() {
-//         document.onmouseup = null;
-//         document.onmousemove = null;
-//     }
-// }
-
-// DragFunc(CardsPro);
